2b1572227c9538814bc47c3cddcf8280c192dd61
Aleksei Bulygin <44240029+AlekseiBulygin@users.noreply.github.com>
2020-10-22 16:47:52 +0300

    Create utils.py

---
+++ network/utils.py
@@ -0,0 +1,8 @@
+import socket
+impmort psutils
+
+def get_ip_addresses():
+    return [snic.address
+            for snics in psutil.net_if_addrs().values()
+            for snic in snics
+            if snic.family == socket.AF_INET]
